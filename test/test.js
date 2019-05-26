0701f09c57df0420084a95593575898b2aa62304
Alec Wilson <alec.wilson@baxter-academy.org>
2019-05-26 17:42:01 -0400

    Setup mocha

---
+++ test/test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+
+const checkCuda = require('../checkCuda');
+
+describe("checkCuda.js", () => {
+    describe("checkCuda", () => {
+        it("Should resolve as true or false", () => {
+            return checkCuda.checkCuda().then((cuda) => {
+                assert.equal(typeof cuda, 'boolean');
+            });
+        });
+    });
+
+    describe("checkCudaSync", () => {
+        it("Should equal true or false", () => {
+            assert.equal(typeof checkCuda.checkCudaSync(), 'boolean');
+        });
+    });
+});
