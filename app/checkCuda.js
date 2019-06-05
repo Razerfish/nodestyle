a425e71bbb539ebd522587665a60ca621436d992
Alec Wilson <alec.wilson@baxter-academy.org>
2019-06-05 13:59:16 -0400

    Update checkCuda.js to use getTorchPath

--- app/checkCuda.js
+++ app/checkCuda.js
@@ -1,5 +1,6 @@
 const execFile = require('child_process').execFile;
 const execFileSync = require('child_process').execFileSync;
+const torchTools = require('./torchTools');
 
 const fs = require('fs');
 const path = require('path');
@@ -13,45 +14,35 @@ const path = require('path');
  */
 function checkCuda() {
     return new Promise((resolve, reject) => {
-        // Find the path to the torchbrain binary.
-        const torchPath = path.join(__dirname, "bin/torchbrain/torchbrain.exe");
+        // Create the process to check if CUDA is available.
+        const cudaProcess = execFile(torchTools.getTorchPath(), ["check_cuda"]);
 
-        // Make sure that the torchbrain binary exists.
-        fs.access(torchPath, fs.constants.F_OK, (err) => {
-            if (err) {
-                reject(err);
-            } else {
-                // Create the process to check if CUDA is available.
-                const cudaProcess = execFile(path.join(__dirname, "bin/torchbrain/torchbrain.exe"), ["check_cuda"]);
+        // Once we get the result from the cudaProcess store it in cudaAvailable.
+        let cudaAvailable;
+        cudaProcess.stdout.on('data', (data) => {
+            cudaAvailable = JSON.parse(data.toString()).data;
+        });
 
-                // Once we get the result from the cudaProcess store it in cudaAvailable.
-                let cudaAvailable;
-                cudaProcess.stdout.on('data', (data) => {
-                    cudaAvailable = JSON.parse(data.toString()).data;
-                });
-
-                // Whenever we get an error, check if it can be parsed as JSON and store it in errors.
-                let errors = [];
-                cudaProcess.stderr.on('data', (error) => {
-                    error.toString().split("\n").forEach((chunk) => {
-                        if (chunk.length > 0) {
-                            try {
-                                errors.push(JSON.parse(chunk).data);
-                            } catch (err) {
-                                errors.push(chunk);
-                            }
-                        }
-                    });
-                });
-
-                cudaProcess.on('exit', (code) => {
-                    // If the exit code is not 0, reject with the errors, otherwise resolve with cudaAvailable.
-                    if (code !== 0) {
-                        reject(errors);
-                    } else {
-                        resolve(cudaAvailable);
+        // Whenever we get an error, check if it can be parsed as JSON and store it in errors.
+        let errors = [];
+        cudaProcess.stderr.on('data', (error) => {
+            error.toString().split("\n").forEach((chunk) => {
+                if (chunk.length > 0) {
+                    try {
+                        errors.push(JSON.parse(chunk).data);
+                    } catch (err) {
+                        errors.push(chunk);
                     }
-                });
+                }
+            });
+        });
+
+        cudaProcess.on('exit', (code) => {
+            // If the exit code is not 0, reject with the errors, otherwise resolve with cudaAvailable.
+            if (code !== 0) {
+                reject(errors);
+            } else {
+                resolve(cudaAvailable);
             }
         });
     });
@@ -66,7 +57,7 @@ function checkCuda() {
  */
 function checkCudaSync(timeout = 10000) {
     // Get output from the check CUDA process.
-    let cuda = execFileSync(path.join(__dirname, "bin/torchbrain/torchbrain.exe"), ["check_cuda"], {
+    let cuda = execFileSync(torchTools.getTorchPath(), ["check_cuda"], {
         timeout: timeout
     });
 
