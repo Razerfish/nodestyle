84031d093a5ab7717c3c09d2889442f48ca93fd3
Alec Wilson <alec.wilson@baxter-academy.org>
2019-06-07 13:58:18 -0400

    Add tests for validateCuda

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 const nodeStyle = require('../main');
+const arguments = require('../app/arguments');
 
 describe("checkCuda.js", () => {
     describe("checkCuda", () => {
@@ -22,6 +23,44 @@ describe("torchTools.js", () => {
     describe("getTorchPath", () => {
         it("Should return a string", () => {
             assert.equal(typeof nodeStyle.torch.getTorchPath(), 'string');
+        });
+    });
+});
+
+describe("arguments.js", () => {
+    describe("validateCuda", () => {
+        it("Should fail if cuda argument is missing", () => {
+            assert.throws(() => {
+                arguments.validateCuda({});
+            }, new Error("The cuda argument is required"));
+        });
+
+        it("Should fail if the cuda argument is not a string", () => {
+            assert.throws(() => {
+                arguments.validateCuda({ cuda: 123456 });
+            }, {
+                name: 'TypeError'
+            });
+        });
+
+        it("Should fail if cuda is not 'yes', 'no' or 'lax'", () => {
+            assert.throws(() => {
+                arguments.validateCuda({ cuda: "fail" });
+            }, {
+                name: 'Error'
+            });
+        });
+
+        it("Should pass if cuda is 'yes'", () => {
+            assert.ifError(arguments.validateCuda({ cuda: 'yes' }));
+        });
+
+        it("Should pass if cuda is 'no'", () => {
+            assert.ifError(arguments.validateCuda({ cuda: 'no' }));
+        });
+
+        it("Should pass if cuda is 'lax'", () => {
+            assert.ifError(arguments.validateCuda({ cuda: 'lax' }));
         });
     });
 });
