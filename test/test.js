4406a4e9b76154f03fb0c8ea4f6c6be87065926b
Alec Wilson <alec.wilson@baxter-academy.org>
2019-06-27 23:24:11 -0400

    Remove mocha.opts
    Removed mocha.opts and replaced it with a per-test timeout.

--- test/test.js
+++ test/test.js
@@ -7,6 +7,7 @@ const nodeStyle = require('../main');
 describe("checkCuda.js", function() {
     describe("checkCuda", function() {
         it("Should resolve as true or false", function() {
+            this.timeout(30000);
             return nodeStyle.checkCuda().then((cuda) => {
                 assert.equal(typeof cuda, 'boolean');
             });
@@ -14,6 +15,7 @@ describe("checkCuda.js", function() {
     });
 
     describe("checkCudaSync", function() {
+        this.timeout(30000);
         it("Should equal true or false", function() {
             assert.equal(typeof nodeStyle.checkCudaSync(), 'boolean');
         });
