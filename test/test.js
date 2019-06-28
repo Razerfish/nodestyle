daeda782a04c078ce4a8021f221e6e25c98fe476
Alec Wilson <alec.wilson@baxter-academy.org>
2019-06-27 23:06:14 -0400

    Update test.js
    Remove arrow function statements from the test.js file.

--- test/test.js
+++ test/test.js
@@ -4,17 +4,17 @@ const assert = require('assert');
 
 const nodeStyle = require('../main');
 
-describe("checkCuda.js", () => {
-    describe("checkCuda", () => {
-        it("Should resolve as true or false", () => {
+describe("checkCuda.js", function() {
+    describe("checkCuda", function() {
+        it("Should resolve as true or false", function() {
             return nodeStyle.checkCuda().then((cuda) => {
                 assert.equal(typeof cuda, 'boolean');
             });
         });
     });
 
-    describe("checkCudaSync", () => {
-        it("Should equal true or false", () => {
+    describe("checkCudaSync", function() {
+        it("Should equal true or false", function() {
             assert.equal(typeof nodeStyle.checkCudaSync(), 'boolean');
         });
     });
