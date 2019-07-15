38f50fa94a56c2833568bf09f3c5529742d4937d
Alec Wilson <alec.wilson@baxter-academy.org>
2019-07-15 17:38:07 -0400

    Create compile.js

---
+++ Scripts/compile.js
@@ -0,0 +1,47 @@
+const ora = require('ora');
+const cp = require('child_process');
+
+
+function main() {
+    return new Promise((resolve, reject) => {
+        const spinner = ora({ prefixText: "Compiling torchbrain", spinner: "line" }).start();
+
+
+        const compile = cp.execFile("./env/Scripts/pyinstaller.exe", ['torchbrain.spec', '--onedir', '--noconfirm', '--workpath=build', '--distpath=app/bin']);
+
+        compile.stdout.on('data', (data) => {
+            spinner.text = data.toString().trim();
+        });
+
+        compile.stderr.on('data', (data) => {
+            spinner.text = data.toString().trim();
+        });
+
+        compile.on('exit', (code) => {
+            if (code === 0) {
+                spinner.text = "";
+                spinner.succeed();
+
+                resolve();
+            } else {
+                spinner.fail();
+
+                reject();
+            }
+        });
+    });
+}
+
+
+if (require.main === module) {
+    main().then(() => {
+        process.exit(0);
+    }).catch(() => {
+        process.exit(1);
+    });
+}
+
+
+module.exports = {
+    main: main
+};
