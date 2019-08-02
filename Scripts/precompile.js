76114df5d864ac1142481840f4fa96a771079399
Alec Wilson <alec.wilson@baxter-academy.org>
2019-08-02 18:42:56 -0400

    Add spinner support to checkModel

--- Scripts/precompile.js
+++ Scripts/precompile.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const os = require('os');
+const ora = require('ora');
 const path = require('path');
 const venv = require('./venv');
 const model = require('./acquireModel');
@@ -12,14 +13,19 @@ require('colors');
  * @description Checks if the vgg16 model is present at ./torchbrain/vgg16-397923af.pth. If the model is not present it either downloads or copies it.
  */
 function checkModel() {
+    const spinner = ora({ prefixText: "Verifing model", spinner: "line" }).start();
+
     if (!fs.existsSync("./torchbrain/vgg16-397923af.pth")) {
+        spinner.fail("Model file is missing\n");
+        console.log("Attempting to create model file...\n");
+
         if (fs.existsSync(path.join(os.homedir(), ".torch/models/vgg16-397923af.pth"))) {
             model.copyModel();
         } else {
             model.downloadModel();
         }
     } else {
-        console.log("Model is present\n".blue);
+        spinner.succeed();
     }
 }
 
